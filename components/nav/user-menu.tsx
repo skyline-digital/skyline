62c9725c80e0b00fc4174ade283b5bc220e6b485
Jack Darlington <100313566+jackdar@users.noreply.github.com>
2024-09-20 22:27:35 +0000

    Merge changes

---
+++ components/nav/user-menu.tsx
@@ -0,0 +1,47 @@
+import { createClient } from "@/utils/supabase/server";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import Image from "next/image";
+import { signOutAction } from "@/app/actions";
+
+export default async function UserMenu() {
+  const supabase = createClient();
+
+  const { data: profile } = await supabase.from("profiles").select().single();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="overflow-hidden rounded-full"
+        >
+          {profile?.avatar ? (
+            <Image
+              src={profile.avatar}
+              width={36}
+              height={36}
+              alt="Avatar"
+              className="overflow-hidden rounded-full"
+            />
+          ) : (
+            <div className="w-9 h-9 bg-accent rounded-full flex items-center justify-center">
+              {profile?.name?.split(" ").map((name) => name[0].toUpperCase())}
+            </div>
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuItem>Support</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <Button variant="ghost" onClick={signOutAction} asChild>
+          <DropdownMenuItem>Logout</DropdownMenuItem>
+        </Button>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
